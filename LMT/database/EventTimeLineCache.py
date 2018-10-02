14ab3598b3e07ef8b1a422bf964b5cbd32a52d4f
fdechaumont <fabrice.de.chaumont@gmail.Com>
2018-10-02 12:15:55 +0200

    add an option to disable the cache (for low memory computer)

--- LMT/database/EventTimeLineCache.py
+++ LMT/database/EventTimeLineCache.py
@@ -6,22 +6,38 @@ Created on 25 sept. 2018
 from database.Event import EventTimeLine
 
 eventCacheDico_={}
+eventCacheEnable_ = True
+
+def disableEventTimeLineCache():
+    ''' If you are using a computer with small amount of RAM, it might be better to disable cache '''
+    eventCacheDico_.clear()
+    global eventCacheEnable_
+    eventCacheEnable_ = False
+    print( "Cache event is disabled" )
     
-def getEventTimeLineCached( connection, file, eventName, idA=None, idB=None, idC=None, idD = None, minFrame=None, maxFrame=None ):
     
+def EventTimeLineCached( connection, file, eventName, idA=None, idB=None, idC=None, idD = None, minFrame=None, maxFrame=None ):
+            
     global eventCacheDico_
     
-    print ("Cache debug:", connection, file, eventName, "ids:" , idA, idB, idC, idD, "frames:", minFrame, maxFrame )
+    if eventCacheEnable_ == True:
     
-    
-    if (file, eventName, idA, idB, idC, idD, minFrame, maxFrame) in eventCacheDico_: 
-        eventTimeLine = eventCacheDico_[ file, eventName, idA, idB, idC, idD, minFrame, maxFrame ]
-        print ( eventName , " Id(",idA ,",", idB, ",", idC, "," , idD , ") Loaded from cache (" , len( eventTimeLine.eventList ) , " records. )")
-        return eventTimeLine
+        print ("Cache debug:", connection, file, eventName, "ids:" , idA, idB, idC, idD, "frames:", minFrame, maxFrame )
+        
+        
+        if (file, eventName, idA, idB, idC, idD, minFrame, maxFrame) in eventCacheDico_: 
+            eventTimeLine = eventCacheDico_[ file, eventName, idA, idB, idC, idD, minFrame, maxFrame ]
+            print ( eventName , " Id(",idA ,",", idB, ",", idC, "," , idD , ") Loaded from cache (" , len( eventTimeLine.eventList ) , " records. )")
+            return eventTimeLine
 
-    print ( "Caching eventTimeLine")
+        
     eventTimeLine = EventTimeLine( connection, eventName , idA = idA, idB = idB, idC = idC, idD = idD, minFrame = minFrame, maxFrame = maxFrame )
-    eventCacheDico_[ file, eventName, idA, idB, idC, idD, minFrame, maxFrame ] = eventTimeLine
+    
+    if eventCacheEnable_ == True:
+        print ( "Caching eventTimeLine")
+        eventCacheDico_[ file, eventName, idA, idB, idC, idD, minFrame, maxFrame ] = eventTimeLine
+    else:
+        print( "Loading event from base (Cache event disabled)")
     
     return eventTimeLine
         
