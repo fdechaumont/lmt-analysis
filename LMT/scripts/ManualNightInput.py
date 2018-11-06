dad24aa4b6b5a396cea6685935311540ee6dbbbe
fdechaumont <fabrice.de.chaumont@gmail.Com>
2018-11-06 12:25:58 +0100

    Class to manually add night event in database

---
+++ LMT/scripts/ManualNightInput.py
@@ -0,0 +1,83 @@
+'''
+Created on 13 sept. 2017
+
+@author: Fab
+'''
+
+import sqlite3
+from database.Animal import *
+import matplotlib.pyplot as plt
+from database.Event import *
+from database.Measure import *
+from database import BuildEventTrain3, BuildEventTrain4, BuildEventTrain2, BuildEventFollowZone, BuildEventRear5, BuildEventFloorSniffing,\
+    BuildEventSocialApproach, BuildEventSocialEscape, BuildEventApproachContact,BuildEventOralOralContact,\
+    BuildEventApproachRear, BuildEventGroup2, BuildEventGroup3, BuildEventGroup4, BuildEventOralGenitalContact, \
+    BuildEventStop, BuildEventWaterPoint, \
+    BuildEventMove, BuildEventGroup3MakeBreak, BuildEventGroup4MakeBreak,\
+    BuildEventSideBySide, BuildEventSideBySideOpposite, BuildEventDetection,\
+    BuildDataBaseIndex, BuildEventWallJump, BuildEventSAP,\
+    BuildEventOralSideSequence, CheckWrongAnimal,\
+    CorrectDetectionIntegrity
+    
+from psutil import virtual_memory
+
+from tkinter.filedialog import askopenfilename
+from database.TaskLogger import TaskLogger
+import sys
+import traceback
+from database.FileUtil import getFilesToProcess
+from database.EventTimeLineCache import flushEventTimeLineCache,\
+    disableEventTimeLineCache
+
+class FileProcessException(Exception):
+    pass
+
+   
+def process( file ):
+    
+    connection = sqlite3.connect( file )     
+
+    print( "--------------")
+    print( "Current file: ", file )
+    print( "Loading existing events...")
+    nightTimeLine = EventTimeLine( connection, "night" , None, None, None , None )        
+    
+    print( "--------------")    
+    print( "Event list:" )
+    for event in nightTimeLine.eventList:
+        print( event )
+    print( "--------------")
+    
+    while True:
+        print( "Manually add night event:")
+        startFrame = input( "Start Frame (return to cancel):" )
+        if not startFrame.isdigit():
+            return
+        
+        endFrame = input( "End Frame (return to cancel):" )
+        if not endFrame.isdigit():
+            return
+        
+        nightTimeLine.addEvent( Event( int(startFrame), int(endFrame) ) )
+        nightTimeLine.endRebuildEventTimeLine(connection)
+
+if __name__ == '__main__':
+    
+    print("Code launched.")
+    
+    files = getFilesToProcess()
+
+    chronoFullBatch = Chronometer("Full batch" )    
+        
+    if ( files != None ):
+    
+        for file in files:
+            try:
+                print ( "Processing file" , file )
+                process( file )
+            except FileProcessException:
+                print ( "STOP PROCESSING FILE " + file , file=sys.stderr  )
+              
+    print( "*** ALL JOBS DONE ***")
+        
+        
