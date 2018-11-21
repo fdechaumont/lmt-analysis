b707a92978543248370d5a2ee9997ed0c07e42df
elodieey <37557881+elodieey@users.noreply.github.com>
2018-11-21 16:11:09 +0100

    Update LMT/scripts/Plot_Trajectory_Single_Object_Explo.py

--- LMT/scripts/Plot_Trajectory_Single_Object_Explo.py
+++ LMT/scripts/Plot_Trajectory_Single_Object_Explo.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     print("Code launched.")
     
     files = askopenfilename( title="Choose a set of file to process", multiple=1 )
-    
+    '''
     for file in files:
         connection = sqlite3.connect( file )
          
@@ -38,3 +38,30 @@ if __name__ == '__main__':
         pool.animalDictionnary[1].plotTrajectory( title = "Second phase " )
         
         plt.show()
+    '''
+    nbFiles = len(files)
+    print(nbFiles)
+    plt.figure( 2*nbFiles, figsize=(13,6*nbFiles) )
+        
+    n = 1
+        
+    for file in files:
+        connection = sqlite3.connect( file )
+             
+        pool = AnimalPool()
+        pool.loadAnimals( connection )
+            
+        #draw the trajectory in the first phase, without the object
+        pool.loadDetection( start=0 , end= 28*oneMinute )
+        plt.subplot(n,2*n,1)
+        pool.animalDictionnary[1].plotTrajectory( show = False, title = "First phase " )
+            
+            
+        #draw the trajectory in the second phase, with the object
+        pool.loadDetection( start=32*oneMinute , end= 60*oneMinute )
+        plt.subplot(n,2*n,2)
+        pool.animalDictionnary[1].plotTrajectory( show = False, title = "Second phase " )
+            
+        n = n+1
+     
+    plt.show()    
