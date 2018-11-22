aaf3dfc9727ea7ca5f2ca5e9d763ca7358ab2c17
fdechaumont <fabrice.de.chaumont@gmail.Com>
2018-11-22 10:58:26 +0100

    added distance between 2 points

--- LMT/database/Util.py
+++ LMT/database/Util.py
@@ -4,7 +4,10 @@ Created on 6 sept. 2017
 @author: Fab
 '''
 
+import math
+
 def level( data ):
+    ''' similar to level in R '''
     dico = {}
     for entry in data:
         dico[entry]=True
@@ -32,3 +35,9 @@ def convert_to_d_h_m_s( frames ):
     days, hours = divmod(hours, 24)
 
     return days, hours, minutes, seconds    
+
+def getDistanceBetweenPointInPx( x1 , y1, x2, y2 ):
+    ''' return the distance between two points in pixel '''
+    distance = math.hypot( x1 - x2, y1 - y2 )    
+    return distance
+    
