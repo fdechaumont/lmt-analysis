fdf4acfbedf44a2e254999aef3024f0f6776e3ec
elodieey <37557881+elodieey@users.noreply.github.com>
2022-04-01 16:28:48 +0200

    Update BuildEventMove.py

--- LMT/lmtanalysis/BuildEventMove.py
+++ LMT/lmtanalysis/BuildEventMove.py
@@ -24,9 +24,9 @@ def flush( connection ):
 
 def reBuildEvent( connection, file, tmin=None, tmax=None, pool = None  ):
     ''' 
-    Animal A is stopped (built-in event):
-    Move social: animal A is stopped and in contact with another animal.
-    Move isolated: animal A is stopped and not in contact with any other animal.
+    Animal A is stopped (built-in event): revert the event to have move events
+    Move social: animal A is moving and in contact with another animal.
+    Move isolated: animal A is moving and not in contact with any other animal.
     ''' 
     
     pool = AnimalPool( )
@@ -64,6 +64,7 @@ def reBuildEvent( connection, file, tmin=None, tmax=None, pool = None  ):
         print('More than one animal in database.')
         isInContactSourceDictionnary = {}
         moveSourceTimeLine = {}
+        moveIsolatedTimeLine = {}
 
         for animal in pool.animalDictionnary.keys():
             ''' Load source stop timeLine and revert it to get the move timeline
@@ -72,8 +73,11 @@ def reBuildEvent( connection, file, tmin=None, tmax=None, pool = None  ):
             moveSourceTimeLine[animal] = EventTimeLine( connection, "Stop", animal, minFrame=tmin, maxFrame=tmax, inverseEvent=True )
             detectionTimeLine = EventTimeLine( connection, "Detection", animal, minFrame=tmin, maxFrame=tmax )
             moveSourceTimeLine[animal].keepOnlyEventCommonWithTimeLine( detectionTimeLine )
-
-            ''' load contact dictionnary with another animal '''
+            
+            #by default let's say that all move are isolated; next we will extract frames which are in contact from this time line.
+            moveIsolatedTimeLine[animal] = moveSourceTimeLine[animal]
+            
+            ''' load contact dictionary with another animal '''
             for animalB in pool.animalDictionnary.keys():
                 if animal == animalB:
                     print('Same identity')
@@ -81,36 +85,40 @@ def reBuildEvent( connection, file, tmin=None, tmax=None, pool = None  ):
                 else:
                     isInContactSourceDictionnary[(animal, animalB)] = EventTimeLineCached( connection, file, "Contact", animal, animalB, minFrame=tmin, maxFrame=tmax ).getDictionnary()
 
-
+        moveIsolatedDic = {}
         for animal in pool.animalDictionnary.keys():
             #initialisation of a dic for isolated move
-            moveIsolatedResult = {}
+            moveIsolatedDic[animal] = moveIsolatedTimeLine[animal].getDictionary()
 
             for animalB in pool.animalDictionnary.keys():
                 # initialisation of a dic for move in contact for each individual of the cage
+                framesToRemoveFromMoveIsolatedTimeLine = []
                 moveSocialResult = {}
                 if animal == animalB:
                     print('Same identity')
                     continue
                 else:
                     ''' loop over eventlist'''
-                    for moveEvent in moveSourceTimeLine[animal].eventList:
+                    for moveEvent in moveIsolatedTimeLine[animal].eventList:
 
                         ''' for each event we seek in t and search a match in isInContactDictionnary between animal and animalB '''
                         for t in range ( moveEvent.startFrame, moveEvent.endFrame+1 ) :
                             if t in isInContactSourceDictionnary[(animal, animalB)]:
                                 moveSocialResult[t] = True
-                            else:
-                                moveIsolatedResult[t] = True
+                                framesToRemoveFromMoveIsolatedTimeLine.append(t)
 
                     ''' save move social at the end of the search for each animal'''
+                    # clean the dictionary of the move and stop events from frames that are overlapping with exclusive contacts
+                    for t in framesToRemoveFromMoveIsolatedTimeLine:
+                        moveIsolatedDic[animal].pop(t, None)
+                        
                     moveSocialResultTimeLine = EventTimeLine( None, "Move in contact" , idA=animal , idB=animalB , idC=None , idD=None , loadEvent=False )
                     moveSocialResultTimeLine.reBuildWithDictionnary( moveSocialResult )
                     moveSocialResultTimeLine.endRebuildEventTimeLine(connection)
 
             ''' save move isolated at the end of the complete process for animalA'''
             moveIsolatedResultTimeLine = EventTimeLine(None, "Move isolated", idA=animal, idB=None, idC=None, idD=None, loadEvent=False)
-            moveIsolatedResultTimeLine.reBuildWithDictionnary(moveIsolatedResult)
+            moveIsolatedResultTimeLine.reBuildWithDictionnary(moveIsolatedDic[animal])
             moveIsolatedResultTimeLine.endRebuildEventTimeLine(connection)
 
 
