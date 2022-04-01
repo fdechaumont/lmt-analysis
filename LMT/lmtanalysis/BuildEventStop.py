0eb18c7edcf48b1f752d2b35e7c923f899b9ba25
elodieey <37557881+elodieey@users.noreply.github.com>
2022-04-01 16:25:09 +0200

    Update BuildEventStop.py

--- LMT/lmtanalysis/BuildEventStop.py
+++ LMT/lmtanalysis/BuildEventStop.py
@@ -24,68 +24,133 @@ def flush( connection ):
 
 def reBuildEvent( connection, file, tmin=None, tmax=None, pool = None ):
     
+    
     ''' 
     Animal A is stopped (built-in event):
-    Stop social: animal A is stopped and in contact with any other animal.
+    Stop in contact: animal A is stopped and in contact with another animal.
     Stop isolated: animal A is stopped and not in contact with any other animal.
     ''' 
     
     pool = AnimalPool( )
     pool.loadAnimals( connection )
-    #pool.loadDetection( start = tmin, end = tmax )
-    
-    isInContactSourceDictionnary = {}
-    stopSourceTimeLine = {}
-    
-    for animal in pool.animalDictionnary.keys():
-        ''' Load source stop timeLine '''
-        stopSourceTimeLine[animal] = EventTimeLineCached( connection, file, "Stop", animal, minFrame=tmin, maxFrame=tmax )
-        for animalB in pool.animalDictionnary.keys():
-            if animal == animalB:
-                print('Same identity')
-                continue
-            else:
-                ''' load contact dictionnary with another animal '''
-                isInContactSourceDictionnary[(animal, animalB)] = EventTimeLineCached( connection, file, "Contact", idA=animal, idB=animalB, minFrame=tmin, maxFrame=tmax ).getDictionnary()
 
-    eventName2 = "Stop in contact"
-    eventName1 = "Stop isolated"        
-    
-    for animal in pool.animalDictionnary.keys():
+    if len(pool.animalDictionnary.keys()) == 1:
+        print('Only one animal in database.')
+        # if the animal has been tested alone, only the stop isolated event will be computed.'''
+        stopSourceTimeLine = {}
 
+        animal = 1
+        # Load source stop timeLine and revert it to get the move timeline
+        # If the animal is not detected, this will result in a move. To avoid this we mask with the detection.
+        
+        stopSourceTimeLine[animal] = EventTimeLine(connection, "Stop", animal, minFrame=tmin, maxFrame=tmax, inverseEvent=False)
+        detectionTimeLine = EventTimeLine(connection, "Detection", animal, minFrame=tmin, maxFrame=tmax)
+        stopSourceTimeLine[animal].keepOnlyEventCommonWithTimeLine(detectionTimeLine)
+
+        # initialisation of a dic for isolated stop
         stopIsolatedResult = {}
-        for animalB in pool.animalDictionnary.keys():
-            stopSocialResult = {}
-            if animal == animalB:
-                print('Same identity')
-                continue
-            else:
-                ''' loop over eventlist'''
-                for stopEvent in stopSourceTimeLine[animal].eventList:
-                    ''' for each event we seek in t and search a match in isInContactDictionnary '''
+
+        # loop over eventlist
+        for stopEvent in stopSourceTimeLine[animal].eventList:
+            for t in range(stopEvent.startFrame, stopEvent.endFrame + 1):
+                stopIsolatedResult[t] = True
+
+
+        # save stop isolated at the end of the complete process for animalA
+        stopIsolatedResultTimeLine = EventTimeLine(None, "Stop isolated", idA=animal, idB=None, idC=None, idD=None, loadEvent=False)
+        stopIsolatedResultTimeLine.reBuildWithDictionnary(stopIsolatedResult)
+        stopIsolatedResultTimeLine.endRebuildEventTimeLine(connection)
+
+
+    else:
+        print('More than one animal in database.')
+        isInContactSourceDictionnary = {}
+        stopSourceTimeLine = {}
+        stopIsolatedTimeLine = {}
+
+        for animal in pool.animalDictionnary.keys():
+            # Load source stop timeLine
+            # If the animal is not detected, this will result in a stop. To avoid this we mask with the detection.
+            
+            stopSourceTimeLine[animal] = EventTimeLine( connection, "Stop", animal, minFrame=tmin, maxFrame=tmax, inverseEvent=False )
+            detectionTimeLine = EventTimeLine( connection, "Detection", animal, minFrame=tmin, maxFrame=tmax )
+            stopSourceTimeLine[animal].keepOnlyEventCommonWithTimeLine( detectionTimeLine )
+            
+            #by default let's say that all stops are isolated; next we will extract frames which are in contact from this time line.
+            stopIsolatedTimeLine[animal] = stopSourceTimeLine[animal]
+            
+            
+            '''
+            a = 2
+            b = a
+            b = 3
+            
+            a = [1,2,3]
+            b = a.copy()
+            b[0] ="choups"
+            a => ["choups",2,3]
+            '''
+            
+            # load contact dictionary with another animal
+            for animalB in pool.animalDictionnary.keys():
+                if animal == animalB:
+                    print('Same identity')
+                    continue
+                else:
+                    isInContactSourceDictionnary[(animal, animalB)] = EventTimeLineCached( connection, file, "Contact", animal, animalB, minFrame=tmin, maxFrame=tmax ).getDictionnary()
+
+        stopIsolatedDic = {}
+        for animal in pool.animalDictionnary.keys():
+            #initialisation of a dic for isolated stop
+            stopIsolatedDic[animal] = stopIsolatedTimeLine[animal].getDictionary()
+
+            for animalB in pool.animalDictionnary.keys():
+                # initialisation of a dic for stop in contact for each individual of the cage
+                framesToRemoveFromStopIsolatedTimeLine = []
+                stopSocialResult = {}
+                if animal == animalB:
+                    print('Same identity')
+                    continue
+                
+                # loop over eventlist
+                for stopEvent in stopIsolatedTimeLine[animal].eventList:
+
+                    # for each event we seek in t and search a match in isInContactDictionnary between animal and animalB
                     for t in range ( stopEvent.startFrame, stopEvent.endFrame+1 ) :
                         if t in isInContactSourceDictionnary[(animal, animalB)]:
                             stopSocialResult[t] = True
-                        else:
-                            stopIsolatedResult[t] = True
-        
-                ''' save stop social '''
-                stopSocialResultTimeLine = EventTimeLine( None, eventName2 , animal , animalB , None , None , loadEvent=False )
+                            framesToRemoveFromStopIsolatedTimeLine.append(t)                        
+
+                # save stop social at the end of the search for each animal
+                # clean the dictionary of the stop events from frames that are overlapping with exclusive contacts
+                if animal == 1 :
+                    print("--- DEBUG")
+                    print( pool.animalDictionnary[animal].RFID , pool.animalDictionnary[animalB].RFID )                
+                    print ( framesToRemoveFromStopIsolatedTimeLine )
+                
+                for t in framesToRemoveFromStopIsolatedTimeLine:
+                    
+                    stopIsolatedDic[animal].pop( t, None )
+                    
+                    
+                stopSocialResultTimeLine = EventTimeLine( None, "Stop in contact" , idA=animal , idB=animalB , idC=None , idD=None , loadEvent=False )
                 stopSocialResultTimeLine.reBuildWithDictionnary( stopSocialResult )
                 stopSocialResultTimeLine.endRebuildEventTimeLine(connection)
 
-        ''' save stop isolated '''
-        stopIsolatedResultTimeLine = EventTimeLine( None, eventName1 , animal , None , None , None , loadEvent=False )
-        stopIsolatedResultTimeLine.reBuildWithDictionnary( stopIsolatedResult )
-        stopIsolatedResultTimeLine.endRebuildEventTimeLine(connection)
+            # save stop isolated at the end of the complete process for animalA
+            stopIsolatedResultTimeLine = EventTimeLine(None, "Stop isolated", idA=animal, idB=None, idC=None, idD=None, loadEvent=False)
+            stopIsolatedResultTimeLine.reBuildWithDictionnary(stopIsolatedDic[animal])
+            stopIsolatedResultTimeLine.endRebuildEventTimeLine(connection)
+
+
 
         
     # log process
     from lmtanalysis.TaskLogger import TaskLogger
     t = TaskLogger( connection )
     t.addLog( "Build Event Stop" , tmin=tmin, tmax=tmax )
-
-                   
+                       
     print( "Rebuild event finished." )
-        
+    
+    
     
