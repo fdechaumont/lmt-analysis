051dfe570c9f47bb4a4660bdfc80867993fe39de
elodieey <37557881+elodieey@users.noreply.github.com>
2022-10-17 17:09:55 +0200

    Update BuildEventFollowZone2.py

--- LMT/lmtanalysis/BuildEventFollowZone2.py
+++ LMT/lmtanalysis/BuildEventFollowZone2.py
@@ -22,50 +22,20 @@ from lmtanalysis.EventTimeLineCache import EventTimeLineCached
 import copy
 from copy import deepcopy
 
-eventName = "FollowZone New2"  # FIX: CHECK IF that's not over propagated
+eventName = "FollowZone New4"
 
-FOLLOW_CORRIDOR_DURATION = 30
-
+FOLLOW_CORRIDOR_DURATION = 30 # in frames
+FOLLOW_MAX_ANGLE = math.pi/4 # in radians
+FOLLOW_DISTANCE_MAX_PIX = 2.5/scaleFactor # numeric value in cm to obtain pixels
+FOLLOW_SPEED_MULTIPLICATOR_THRESHOLD = 2
+FOLLOW_REMOVE_EVENT_BELOW_LEN = 7 # in frames        
+FOLLOW_MERGE_EVENT_LEN_CRITERIA = 10 # in frames
+            
 def flush( connection ):
     ''' flush event in database '''
+    print("Flushing " , eventName )
     deleteEventTimeLineInBase(connection, eventName )
 
-'''
-def line( x1, x2 , y1, y2 ):
-    x1, y1 = [x1, y1], [x2, y2]    
-    plt.plot(x1, y1, marker = 'o')    
-
-
-def checkZone ( massA_x, massA_y, angleA, width, length, massB_x, massB_y, angleB ):
-
-    dbDif= math.atan2( math.sin(angleB-angleA), math.cos(angleB-angleA) )
-    dif = math.fabs( dbDif ) # FIX: CheCK IF THIS DHOULD BE THERE
-
-    if ( dif < math.pi/4):
-        #transfer the referential: position of B in the new space
-        x,y = transformPoint(angleB, massA_x, massA_y, massB_x, massB_y)
-        #print(x,y)
-        
-        #if (x>-0.5*width and x<0.5*width and y>-3*length and y<0):  # FIX > est ce qu'on est bien dans la transfo ?
-        if (x>-0.5*width and x<0.5*width and y>-0.5*width and y<0):  # FIX > est ce qu'on est bien dans la transfo ?
-        #if ( x >-20 and x < 20 and y > -100 and y < 0 ):
-            return True
-
-    
-def transformPoint(angleB, massA_x, massA_y, massB_x, massB_y):
-    
-    #transform the origin of the space so that the mass center of animalB is the origin of the new space
-        
- 
-    Affine.identity()
-
-    x = massA_x - massB_x
-    y = massA_y - massB_y
-
-    rotation = Affine.rotation(90- math.degrees( angleB ) ) * ( x , y )
-    
-    return rotation[0], rotation[1]
-'''
 
 def isSameWay( detA, detB ):
     
@@ -82,7 +52,7 @@ def isSameWay( detA, detB ):
     
     return False
 
-def isAFollowingB( t , animalA , dicA , animalB , dicB ):
+def isAFollowingB( t , dicA , dicB ):
     
     # False is detection A does not exist at t
     if t not in dicA:
@@ -108,9 +78,13 @@ def isAFollowingB( t , animalA , dicA , animalB , dicB ):
         detectionB = dicB[timeB]
         
         distance = detectionA.getDistanceTo( detectionB )  
-        
+
+        # discard invalid distances
+        if distance == None:
+            continue
+                
         # discard if distance is too large between detections
-        if distance > 10: # PLEASE FIX: THIS IS TO DEFINE
+        if distance > FOLLOW_DISTANCE_MAX_PIX:
             continue
                          
         # discard if animals are not going the same way ( scalar test )
@@ -122,38 +96,12 @@ def isAFollowingB( t , animalA , dicA , animalB , dicB ):
         angleB = detectionB.getDirection()
         dbDif= math.atan2( math.sin(angleB-angleA), math.cos(angleB-angleA) )
         dif = math.fabs( dbDif )
-        if ( dif > math.pi/4):
+        if ( dif > FOLLOW_MAX_ANGLE ):
             continue
         
         
         return True
         
-    '''
-    
-    if t in contactDic[idAnimalA, idAnimalB]:
-                    continue
-                                
-                
-                for time in range( t-animalA.parameters.FOLLOW_CORRIDOR_DURATION, t+1, 2):
-                    
-                    # does A exists at tested time ?
-                    if not time in dicA:
-                        continue
-                
-                    # is a in the track of B ?
-                    try:
-                        angleA = pool.animalDictionnary[idAnimalA].getDirection(time)
-                        angleB = pool.animalDictionnary[idAnimalB].getDirection(time)
-                    except:
-                        continue
-                    
-                    if ( checkZone( dicA[time].massX, dicA[time].massY, angleA, 
-                                        animalA.parameters.FOLLOW_CORRIDOR_WIDTH, animalA.parameters.FOLLOW_CORRIDOR_LENGTH, 
-                                        dicB[time].massX, dicB[time].massY, angleB ) == True):
-                        resultDic[t]=True
-                        break
-    
-    '''
     
     
     
@@ -186,7 +134,8 @@ def reBuildEvent( connection, file, tmin=None, tmax=None, pool = None ):
     
     # filter detection by speed. Keep only the detection that are over SPEED_THRESHOLD_LOW.
     #pool.filterDetectionByInstantSpeed( pool.getAnimalList()[0].parameters.SPEED_THRESHOLD_LOW , 1000 ) # for the rat compatible version
-    pool.filterDetectionByInstantSpeed( SPEED_THRESHOLD_LOW*2 , 1000 )
+    #pool.filterDetectionByInstantSpeed( SPEED_THRESHOLD_LOW*2 , 1000 )
+    pool.filterDetectionByInstantSpeed( SPEED_THRESHOLD_LOW*FOLLOW_SPEED_MULTIPLICATOR_THRESHOLD , 1000 )
     
     # remove all detection where head and tail are missing
     pool.filterDetectionToKeepOnlyHeadTailDetection()
@@ -230,16 +179,10 @@ def reBuildEvent( connection, file, tmin=None, tmax=None, pool = None ):
             animalB = pool.animalDictionnary[idAnimalB]
             dicB = animalB.detectionDictionnary
             
-            '''
-            # discard if animals are currently in contact at t
-            if t in contactDic[idAnimalA, idAnimalB]:
-                continue
-            '''
-                
             # Starting "is A following B ?"
                         
             for t in dicB:            
-                if isAFollowingB( t , animalA , dicA , animalB , dicB ):
+                if isAFollowingB( t , dicA , dicB ):
                     resultDic[t]=True
 
             # remove all contact situation
@@ -247,121 +190,8 @@ def reBuildEvent( connection, file, tmin=None, tmax=None, pool = None ):
                 if t in resultDic:
                     resultDic.pop( t )
             
-            '''
-            
-            # test if B is followed by A at t
-            for t in dicB:
-                
-                if t in contactDic[idAnimalA, idAnimalB]:
-                    continue
-                                
-                
-                for time in range( t-animalA.parameters.FOLLOW_CORRIDOR_DURATION, t+1, 2):
-                    
-                    # does A exists at tested time ?
-                    if not time in dicA:
-                        continue
-                
-                    # is a in the track of B ?
-                    try:
-                        angleA = pool.animalDictionnary[idAnimalA].getDirection(time)
-                        angleB = pool.animalDictionnary[idAnimalB].getDirection(time)
-                    except:
-                        continue
-                    
-                    if ( checkZone( dicA[time].massX, dicA[time].massY, angleA, 
-                                        animalA.parameters.FOLLOW_CORRIDOR_WIDTH, animalA.parameters.FOLLOW_CORRIDOR_LENGTH, 
-                                        dicB[time].massX, dicB[time].massY, angleB ) == True):
-                        resultDic[t]=True
-                        break
-            '''
             followIsolatedTimeLineDic[idAnimalA, idAnimalB] = resultDic
 
-    '''
-    for idAnimalA in pool.animalDictionnary:
-        for idAnimalB in pool.animalDictionnary:
-            if( idAnimalA == idAnimalB ):
-                continue
-            resultIsoDic = {}
-            
-            animalA = pool.animalDictionnary[idAnimalA]
-            animalB = pool.animalDictionnary[idAnimalB]
-
-            dicA = animalA.detectionDictionnary
-            dicB = animalB.detectionDictionnary
-                    
-            for t in dicB:
-                
-                debug  = False
-                if t==  354:
-                    debug = True
-                    print( f"A:{animalA}")
-                    print( f"B:{animalB}")
-                
-                if t in contactDic[idAnimalA, idAnimalB]:
-                    continue
-                                                               
-                if not t in dicA:
-                    continue
-                
-                speedA = pool.animalDictionnary[idAnimalA].getSpeed(t)
-                
-                if debug:
-                    print( f"SpeedA: {speedA}" )
-                #speedB = pool.animalDictionnary[idAnimalB].getSpeed(t)
-                #angleB = pool.animalList[idAnimalB].getDirection(t)
-                
-                if speedA == None:
-                    if debug:
-                        print( f"SpeedA none: {speedA}" )
-                    continue
-
-                if speedA < animalA.parameters.SPEED_THRESHOLD_LOW:
-                    if debug:
-                        print( f"SpeedA too low: {speedA}" )
-                    continue
-                
-                angleA = pool.animalDictionnary[idAnimalA].getDirection(t)
-                if debug:
-                    print( f"angleA : {angleA}" )
-                    
-                for time in range( t-animalA.parameters.FOLLOW_CORRIDOR_DURATION, t+1, 2):
-                    
-                    if debug:
-                        print( f"time: {time} ------------------------" )
-                                        
-                    if not time in dicB:                        
-                        continue
-                    
-                    speedB = pool.animalDictionnary[idAnimalB].getSpeed(time)
-                    if speedB == None:
-                        continue
-                    
-                    if speedB < animalB.parameters.SPEED_THRESHOLD_LOW:
-                        continue
-                    
-                    angleB = None
-                    if time in pool.animalDictionnary[idAnimalB].detectionDictionnary:
-                        angleB = pool.animalDictionnary[idAnimalB].detectionDictionnary[time].getDirection()
-                        
-                    if angleB == None:
-                        continue
-
-                    if debug:
-                        print( f"angleB: {angleB}" )
-
-                    if ( checkZone( dicA[t].massX, dicA[t].massY, angleA, 
-                                        animalA.parameters.FOLLOW_CORRIDOR_WIDTH, animalA.parameters.FOLLOW_CORRIDOR_LENGTH, 
-                                        dicB[time].massX, dicB[time].massY, angleB ) == True):
- 
-                        resultIsoDic[t] = True
-                        if debug:
-                            print( f"MATCH: A:{t} B:{time}" )
-                        break
-            
-            
-            
-    '''
             
     #rebuild timelines with dictionaries
     for idAnimalA in pool.animalDictionnary:
@@ -369,12 +199,11 @@ def reBuildEvent( connection, file, tmin=None, tmax=None, pool = None ):
             if( idAnimalB == idAnimalA ):
                 continue
             followIsolatedTimeLine[idAnimalA, idAnimalB].reBuildWithDictionnary( followIsolatedTimeLineDic[idAnimalA, idAnimalB] )
-            
-            # FIXME: those 2 smoothing filters could be at call process. Design choice needed.
+                        
             # filter out accidental events
-            followIsolatedTimeLine[idAnimalA, idAnimalB].removeEventsBelowLength( 7 )
+            followIsolatedTimeLine[idAnimalA, idAnimalB].removeEventsBelowLength( FOLLOW_REMOVE_EVENT_BELOW_LEN )
             # create continuity in the events
-            followIsolatedTimeLine[idAnimalA, idAnimalB].mergeCloseEvents( 10 )
+            followIsolatedTimeLine[idAnimalA, idAnimalB].mergeCloseEvents( FOLLOW_MERGE_EVENT_LEN_CRITERIA )
                         
             followIsolatedTimeLine[idAnimalA, idAnimalB].endRebuildEventTimeLine(connection)
                 
