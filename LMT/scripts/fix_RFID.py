bbebd4720246fcbc64e6aec240888b9071ba2f0a
fdechaumont <fabrice.de.chaumont@gmail.Com>
2022-10-26 12:28:01 +0200

    fix if no detection is found at all

--- LMT/scripts/fix_RFID.py
+++ LMT/scripts/fix_RFID.py
@@ -1,5 +1,5 @@
 '''
-Created on 25 oct. 2022
+Created on 25 Oct 2022
 
 @author: Fab
 '''
@@ -8,11 +8,12 @@ from lmtanalysis.Animal import AnimalPool
 import sqlite3
 
 def query( connection , query ):
+    print( query )
     cursor = connection.cursor()
     cursor.execute( query )
     connection.commit()
     cursor.close()
-    
+
 
 def process( file ):
 
@@ -22,8 +23,8 @@ def process( file ):
     connection = sqlite3.connect( file )        
     animalPool = AnimalPool( )
     animalPool.loadAnimals( connection )
-    
-    
+
+
     for animal in animalPool.getAnimalList():
         if not animal.RFID.isnumeric():
             print("----")
@@ -32,65 +33,68 @@ def process( file ):
             print("Loading its detection:")
             animal.loadDetection( lightLoad=True )
             print("Animal number of detection: " , len( animal.detectionDictionnary ) )
-            firstDetectionT = sorted(animal.detectionDictionnary.keys())[0]
-            lastDetectionT = sorted(animal.detectionDictionnary.keys())[-1]
-            print("First detection: ", firstDetectionT )
-            print("Last detection: ", lastDetectionT )
-                        
+            try:
+                firstDetectionT = sorted(animal.detectionDictionnary.keys())[0]
+                lastDetectionT = sorted(animal.detectionDictionnary.keys())[-1]
+                print("First detection: ", firstDetectionT )
+                print("Last detection: ", lastDetectionT )
+            except:
+                print("No detection associated")
+
             answer = input("Remove animal and IDs ? [y/n]")
             if answer.lower() == "y":
                 print("Removing animal, detection and events corresponding to this animal")
-                
+
                 query( connection, f"DELETE FROM 'animal' WHERE id={animal.baseId}" )                
                 query( connection, f"DELETE FROM 'detection' WHERE animalid={animal.baseId}" )                
                 query( connection, f"DELETE FROM 'event' WHERE idanimala={animal.baseId}" )
                 query( connection, f"DELETE FROM 'event' WHERE idanimalb={animal.baseId}" )
                 query( connection, f"DELETE FROM 'event' WHERE idanimalc={animal.baseId}" )
                 query( connection, f"DELETE FROM 'event' WHERE idanimald={animal.baseId}" )
-                
 
-    
+
+
     print("--------")
-    
+
     while True:
         # reconnect
         connection = sqlite3.connect( file )        
         animalPool = AnimalPool( )
         animalPool.loadAnimals( connection )
-         
+
         answer = input("Do you want to update an id of an animal ? (y/n)")
         if answer.lower() == "n":
             break
-        
+
         if answer.lower() == "y":
-            
+
             answer = input("What id do you want to move ?")
             sourceid = answer
-            
+
             answer = input("To which id ?")
             targetid = answer
-                        
+
             query( connection, f"UPDATE 'animal' set id={targetid} where id={sourceid}" )
             query( connection, f"UPDATE 'detection' set animalid={targetid} where animalid={sourceid}" )
             query( connection, f"UPDATE 'event' set idanimala={targetid} where idanimala={sourceid}" )
             query( connection, f"UPDATE 'event' set idanimalb={targetid} where idanimalb={sourceid}" )
             query( connection, f"UPDATE 'event' set idanimalc={targetid} where idanimalc={sourceid}" )
             query( connection, f"UPDATE 'event' set idanimald={targetid} where idanimald={sourceid}" )
-            
-                
-            
-            
-        
-        
-    
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    
+
     print("This code will fix RFID problem such as RFID_A present in your experiment")
-    
+
     files = getFilesToProcess()
-    
+
     for file in files:
         process( file )
 
